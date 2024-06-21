633f8aa12ec4667157d8a59ef6bbb9b8e654be48
kapy0312 <kapy0312@gmail.com>
2024-06-21 16:06:35 +0800

    20240621_v1.1

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const puzzleContainer = document.getElementById('puzzle-container');
     let imgArray = ["img/範例圖片1.png", "img/範例圖片2.png", "img/範例圖片3.png", "img/範例圖片4.png", "img/範例圖片5.png", "img/範例圖片6.png", "img/範例圖片11.gif", "img/範例圖片12.gif"];
-    let imageUrl = getRandomElement(imgArray); // 替换为你的图片路径
+    let imageUrl = imgArray[0]; // 替换为你的图片路径
 
     // 隨機抽取一個陣列元素
     function getRandomElement(array) {
         const randomIndex = Math.floor(Math.random() * array.length);
         return array[randomIndex];
     }
-    
+
     const uploadInput = document.getElementById('upload');
     let puzzlePieces = Array.from({ length: 8 }, (_, i) => i + 1);
     puzzlePieces.push(null); // 空白塊
@@ -29,7 +29,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+    // 處理圖片上傳
+    $('#RandomButton').click(function () {
+        imageUrl = getRandomElement(imgArray); // 替换为你的图片路径
 
+        shuffle(puzzlePieces);
+        createPieces();
+    });
 
     shuffle(puzzlePieces);
 
@@ -56,7 +62,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 piece.style.backgroundImage = `url(${imageUrl})`;
                 const row = Math.floor((number - 1) / 3);
                 const col = (number - 1) % 3;
-                piece.style.backgroundPosition = `-${col * 100}px -${row * 100}px`;
+                piece.style.backgroundPosition = `-${col * 200}px -${row * 200}px`;
             }
             piece.dataset.index = index;
             puzzleContainer.appendChild(piece);
